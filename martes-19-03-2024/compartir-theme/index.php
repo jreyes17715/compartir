e2a37639bb5cf8f0f8ea268226127c41c2414657
Jonatan Reyes <jreyes.reyes17715@gmail.com>
2024-03-19 23:17:23 -0600

    version final 1

---
+++ martes-19-03-2024/compartir-theme/index.php
@@ -0,0 +1,126 @@
+<?php 
+/*template name: inicio*/
+get_header();
+?>
+
+
+    <?php 
+        require('templates/descubre.php');
+     ?>
+
+    <!---- SECTION NUESTRA PROPUESTA ---->
+    <?php 
+        require('templates/nuestra-propuesta.php');
+     ?>
+
+    <!-- #SECTION NUESTRA PROPUESTA -->
+
+    <!---SECTION EVALUAR PARA APRENDER----->
+   <?php 
+        require('templates/evaluar.php');
+     ?>
+    <!------SECTION EVALUAR PARA APRENDER---------->
+    <!-----------ACOMPAÑAMIENTO---------------->
+    <?php 
+        require('templates/acompanamiento.php');
+     ?>
+
+     <?php 
+        require('templates/modal.php');
+     ?>
+    
+    
+    <!-----------ACOMPAÑAMIENTO---------------->
+    <footer class="footer">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-blanco.png" alt="">
+       <script>
+            const hamburger = document.querySelector(".hamburger");
+            const navMenu = document.querySelector(".nav-menu");
+
+            hamburger.addEventListener("click", () => {
+
+            /* Toggle active class */
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+
+            /* Toggle aria-expanded value */
+            let menuOpen = navMenu.classList.contains("active");
+            console.log(menuOpen)
+            let newMenuOpenStatus = menuOpen;
+            hamburger.setAttribute("aria-expanded", newMenuOpenStatus);
+            })
+
+            // close mobile menu
+            document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+            //   Need to add Toggle aria-expanded value here as well because it stays as true when you click a menu item
+            }))
+
+
+            document.addEventListener('DOMContentLoaded', function() {
+               // Función para cambiar el color de fondo del menú cuando se llega a la sección específica
+                function changeMenuBackground() {
+                    const section = document.getElementById('propuesta'); // Cambiar 'section2' por el ID de la sección deseada
+                    const sectionTop = section.offsetTop - 50;
+                    const sectionBottom = sectionTop + section.offsetHeight;
+                    const currentScroll = window.pageYOffset;
+
+                    const menu = document.getElementById('headContent');
+                    if (currentScroll >= sectionTop && currentScroll < sectionBottom) {
+                        menu.style.backgroundColor = '#211D35'; // Cambiar el color de fondo del menú
+                    } else {
+                       menu.style.backgroundColor = '#fff'; // Restaurar el color de fondo original del menú
+                    }
+                }
+
+                // Escuchar el evento scroll
+                window.addEventListener('scroll', changeMenuBackground);
+
+
+                /*SECCTION SCROLL*/
+            function smoothScroll(target, duration) {
+                const targetElement = document.querySelector(target);
+                
+                const targetPosition = targetElement.offsetTop-100;
+                const startPosition = window.pageYOffset;
+                
+                
+                const distance = targetPosition - startPosition;
+                let startTime = null;
+                
+                function animation(currentTime) {
+                    if (startTime === null) {
+                        startTime = currentTime;
+                    }
+                    const timeElapsed = currentTime - startTime;
+                    const run = ease(timeElapsed, startPosition, distance, duration);
+                    window.scrollTo(0, run);
+                    if (timeElapsed < duration) {
+                        requestAnimationFrame(animation);
+                    }
+                }
+
+            // Función de aceleración
+            function ease(t, b, c, d) {
+                    t /= d / 2;
+                    if (t < 1) return c / 2 * t * t + b;
+                    t--;
+                    return -c / 2 * (t * (t - 2) - 1) + b;
+                }
+
+                requestAnimationFrame(animation);
+            }
+
+            // Agregar evento click a los enlaces del menú
+                document.querySelectorAll('nav a').forEach(anchor => {
+                    anchor.addEventListener('click', function(e) {
+                        e.preventDefault();
+                        const targetId = this.getAttribute('href');
+                        smoothScroll(targetId, 1000); // Duración del scroll suave: 1000ms (1 segundo)
+                    });
+                });
+            });
+        </script>
+    </footer>
+
---
+++ martes-19-03-2024/compartir-theme/templates/nuestra-propuesta.php
@@ -0,0 +1,77 @@
+  <section class="section-our-proposal flex flex-col items-center bg-indigo-950 md:h-full" id="propuesta">
+        <div class="container natives">
+            <div class="flex flex-col items-center">
+                <div class="Title w-full text-center flex justify-center md:justify-start md:mt-20 lg:mt-0">
+                    <h1 class="w-10/12 text-4xl lg:text-5xl xl:text-6xl text-white mt-40  md:mt-20 font-bold md:ms-10 md:w-28 lg:ps-14 lg:mt-32 redhatdisplay-bold">NUESTRA PROPUESTA</h1>
+                </div>
+                <div class="box-img mx-3 my-5 hidden lg:flex md:absolute z-10 h-auto md:h-full flex  lg:mt-24 -lg:ms-32 xl:items-end">
+                    <button class="h-full btn-img flex items-center" onclick="openModal()">
+                        <img class="img-principal lg:-mt-32 xl:mt-20" src="<?php echo get_template_directory_uri(); ?>/assets/images/drg.svg" alt="">
+                    </button>
+                    
+                </div>
+                <div class="flex flex-row w-full justify-center md:justify-end h-auto lg:h-36 xl:pe-36 xxl:pe-48">
+                    <div class="first-card w-11/12 md:w-96 p-5 rounded-2xl relative h-60 flex items-center justify-center md:-mt-32 lg:me-10">
+                        
+                        <div class="container container-card relative px-2 h-full w-full">
+                            <h1 class="text-white font-semibold text-2xl redhatdisplay-bold">SECUENCIA DIDÁCTICA</h1>
+                            <ul class="list-disc ms-5 my-4 righteous-font">
+                                <li class="text-white">Activo</li>
+                                <li class="text-white">Aprendo</li>
+                                <li class="text-white">Aplico</li>
+                            </ul>
+                            <div class="box-buttons absolute flex my-2 z-10">
+                                <button class="bg-purple-600 px-3 py-2 rounded-3xl text-white text-semibold flex me-2 text-xs items-center w-48"><img class="px-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/libro-abierto.svg" alt="">Descargar presentación</button>
+                                <button class="bg-purple-600 w-28 px-3 py-2 rounded-3xl text-white text-semibold flex mx-2 text-xs items-center"><img class="px-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/video.svg" alt="">Ver video</button>
+        
+                            </div>
+                        </div>
+                        <div class="box-background hover:bg-slate-200 opacity-10 absolute w-full h-full top-0 left-0 rounded-md hover:border hover:border-zinc-300"></div> 
+                    </div>
+                </div>
+                <div class="flex flex-row w-full justify-center md:justify-between lg:ms-10">
+                    <div class="first-card w-11/12 md:w-96 p-5 rounded-2xl relative h-60 flex items-center justify-center xl:ms-12 md:mt-10 xl:-mt-10">
+                        
+                        <div class="container container-card relative px-2 h-full w-full">
+                            <h1 class="text-white font-semibold text-2xl redhatdisplay-bold">NEUROPEDAGOGÍA</h1>
+                            <ul class="list-disc ms-5 my-4 righteous-font">
+                                <li class="text-white">Codificación</li>
+                                <li class="text-white">Almacenamiento</li>
+                                <li class="text-white">Evocación</li>
+                            </ul>
+                            <div class="box-buttons absolute flex my-2 z-10">
+                                <button class="bg-violet-400 px-3 py-2 rounded-3xl text-white text-semibold flex me-2 text-xs items-center w-48"><img class="px-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/libro-abierto.svg" alt="">Descargar presentación</button>
+                                <button class="bg-violet-400 w-28 px-3 py-2 rounded-3xl text-white text-semibold flex mx-2 text-xs items-center"><img class="px-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/video.svg" alt="">Ver video</button>
+                                
+                            </div>
+                        </div>
+                        <div class="box-background hover:bg-slate-200 opacity-10 absolute w-full h-full top-0 left-0 rounded-md hover:border hover:border-zinc-300"></div>
+                        
+                    </div>
+                    
+                </div>
+                <div class="flex flex-row w-full justify-center md:justify-end md:h-10">
+                    <div class="first-card w-11/12 md:w-96 p-5 rounded-2xl relative h-60 flex items-center justify-center md:-mt-60 lg:-mt-36 lg:me-10 xl:me-0">
+                        
+                        <div class="container container-card relative px-2 h-full w-full">
+                            <h1 class="text-white font-semibold text-2xl redhatdisplay-bold">EVALUACIÓN</h1>
+                            <ul class="list-disc ms-5 my-4 righteous-font">
+                                <li class="text-white">Diagnóstico</li>
+                                <li class="text-white">Evoca</li>
+                                <li class="text-white">Prueba</li>
+                                <li class="text-white">Refuerza</li>
+                            </ul>
+                            <div class="box-buttons absolute flex my-2 z-10">
+                                <button class="bg-pink-300 px-3 py-2 rounded-3xl text-white text-semibold flex me-2 text-xs items-center w-48"><img class="px-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/libro-abierto.svg" alt="">Descargar presentación</button>
+                                <button class="bg-pink-300 w-28 px-3 py-2 rounded-3xl text-white text-semibold flex mx-2 text-xs items-center"><img class="px-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/video.svg" alt="">Ver video</button>
+        
+                            </div>
+                        </div>
+                        <div class="box-background hover:bg-slate-200 opacity-10 absolute w-full h-full top-0 left-0 rounded-md hover:border hover:border-zinc-300"></div>
+                        
+                    </div>
+            </div>
+            </div>
+
+        </div>
+    </section>
